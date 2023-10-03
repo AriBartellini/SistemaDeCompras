e817d78688e96eda3188562729d23ece193df495
AriBartellini <aribartellini@gmail.com>
2023-10-03 18:49:28 -0300

    Clases entidad completas

--- SistemaDeCompras/src/sistemadecompras/entidades/Producto.java
+++ SistemaDeCompras/src/sistemadecompras/entidades/Producto.java
@@ -1,14 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemadecompras.entidades;
 
-/**
- *
- * @author Ari
- */
 public class Producto {
+    private int idProducto;
+    private String nombreProducto;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private boolean estado;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nombreProducto, String descripcion, double precio, int stock, boolean estado) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombreProducto=" + nombreProducto + ", descripcion=" + descripcion + ", precio=" + precio + ", stock=" + stock + ", estado=" + estado + '}';
+    }
+    
     
 }
--- SistemaDeCompras/src/sistemadecompras/entidades/Proveedor.java
+++ SistemaDeCompras/src/sistemadecompras/entidades/Proveedor.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sistemadecompras.entidades;
 
-/**
- *
- * @author Ari
- */
 public class Proveedor {
+    private int idProveedor;
+    private String nombreProveedor;
+    private String domicilio;
+    private String telefono;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idProveedor, String nombre, String domicilio, String telefono) {
+        this.idProveedor = idProveedor;
+        this.nombreProveedor = nombre;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "idProveedor=" + idProveedor + ", nombre=" + nombreProveedor + ", domicilio=" + domicilio + ", telefono=" + telefono + '}';
+    }
+    
     
 }
