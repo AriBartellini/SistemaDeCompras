6aef42dbad1baefba46047973f327d879e00ff56
dominguezRR <dominguez-rroberto@hotmail.com>
2023-10-03 20:04:37 -0300

    subo ProductoData

--- SistemaDeCompras/src/sistemadecompras/acceso/ProductoData.java
+++ SistemaDeCompras/src/sistemadecompras/acceso/ProductoData.java
@@ -1,5 +1,82 @@
 package sistemadecompras.acceso;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import sistemadecompras.entidades.Producto;
+
 public class ProductoData {
 
-}
+    private Connection con;
+
+    public ProductoData() {
+        con = Conexion.buscarConexion();
+        
+    }
+
+    public void agregrarProducto(Producto producto) {
+
+        String sql = "INSERT INTO Producto (nombre, descripcion, precio, estado) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1,producto.getNombre());
+            ps.setString(2,producto.getDescripcion());
+            ps.setDouble(3,producto.getPrecio());
+            ps.setBoolean(4,producto.isActivo());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                producto.setIdProducto(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Producto Agregado correctamente");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Producto Duplicado");
+        }
+
+    }
+
+
+public void modificarProducto(Producto producto) {
+
+        String sql = "UPDATE Producto SET nombre = ?, descripcion = ? , precio = ?, estado = ? WHERE idMateria = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, producto.getNombre());
+            ps.setString(2, producto.getDescripcion());
+            ps.setDouble(3, producto.getPrecio());
+            ps.setBoolean(4, producto.isActivo());
+            
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Producto modificado correctamente");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder al Producto ");
+        }
+
+    }
+
+public void eliminarProductoPorId(int id) {
+        try {
+            String sql = "UPDATE Producto SET estado = 0 WHERE idProducto = ? ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, "Se eliminó el Producto");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder al Producto");
+        }
+    }
