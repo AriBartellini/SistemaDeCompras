326f5f2227adb5387873fcb9ed6cd963dcf466b2
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-10-03 18:37:36 -0300

    a

--- SistemaDeCompras/src/sistemadecompras/acceso/Conexion.java
+++ SistemaDeCompras/src/sistemadecompras/acceso/Conexion.java
@@ -1,5 +1,34 @@
 package sistemadecompras.acceso;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion {
 
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidad";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection conexion = null;
+    
+    public Conexion() {
+    }
+
+    public static Connection buscarConexion() {  //en el video sale como getConnection
+         
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, " No ha sido posible la conexión con la BD" + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
+            }
+
+        }
+        return conexion;
+    }
 }
--- SistemaDeCompras/src/sistemadecompras/acceso/ProveedorData.java
+++ SistemaDeCompras/src/sistemadecompras/acceso/ProveedorData.java
@@ -1,5 +1,13 @@
 package sistemadecompras.acceso;
 
+import java.sql.Connection;
+
 public class ProveedorData {
 
+    private Connection con;
+    
+    public ProveedorData(){
+        con = Conexion.buscarConexion();
+    }
+    
 }
