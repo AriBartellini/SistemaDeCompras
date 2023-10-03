6bf1686bf4b7769d3203b7689d41e5c28ef65c14
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-10-03 19:59:24 -0300

    Update ProveedorData.java

--- SistemaDeCompras/src/sistemadecompras/acceso/ProveedorData.java
+++ SistemaDeCompras/src/sistemadecompras/acceso/ProveedorData.java
@@ -1,7 +1,6 @@
 package sistemadecompras.acceso;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,22 +25,79 @@ public class ProveedorData {
             ps.setInt(1, proveedor.getIdProveedor());
             ps.setString(2, proveedor.getNombreProveedor());
             ps.setString(3, proveedor.getDomicilio());
-            ps.setString(4, proveedor.getDomicilio());
+            ps.setString(4, proveedor.getTelefono());
            
             ps.executeUpdate();
 
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
                 proveedor.setIdProveedor(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, " guardado correctamente");
+                JOptionPane.showMessageDialog(null, "Proveedor guardado correctamente");
             }
             ps.close();
 
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "alumno duplicado, inserte otro DNI");
+            JOptionPane.showMessageDialog(null, "Proveedor duplicado, inserte otro ID");
         }
 
     }
+    
+    public void modificarProveedor(Proveedor proveedor){
+        
+        String sql = "UPDATE proveedor SET nombre = ?, domicilio = ?, telefono = ? WHERE idProveedor = ? ";
+                
+
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setString(1, proveedor.getNombreProveedor());
+            ps.setString(2, proveedor.getDomicilio());
+            ps.setString(3, proveedor.getTelefono());
+            ps.setInt(4, proveedor.getIdProveedor());
+            
+
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Proveedor modificado correctamente");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla en el metodo modificarProveedor");
+        }
+
+        
     }
     
-}
+    public Proveedor buscarProveedor(){ //busqueda si o si por ID
+        
+        String sql = "SELECT dni, apellido, nombre, fechaNac FROM proveedor WHERE idProveedor = ? AND estado=1";
+        Proveedor proveedor = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                proveedor = new Proveedor();
+                proveedor.setIdProveedor(id);
+                proveedor.setDni(rs.getInt("dni"));
+                proveedor.setApellido(rs.getString("apellido"));
+                proveedor.setNombre(rs.getString("nombre"));
+                
+                
+            } else {
+                JOptionPane.showMessageDialog(null, "No existe un proveedor con ese id");
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla proveedor");
+        }
+        return proveedor;
+        
+    }
+    
+    }
+    
+
