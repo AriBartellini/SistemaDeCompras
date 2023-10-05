75956e1e59a56699a915cd09494e7d56371893f9
AriBartellini <aribartellini@gmail.com>
2023-10-05 12:31:45 -0300

    FUNCIONA LISTAR PROVEEDORES Y EL BOTON MODIFICAR, ELIMINAR NO FUNCIONA TODAVIA PORQUE NECESITO EL DELETE EN LA CLASE DATA

--- SistemaDeCompras/src/sistemadecompras/vistas/ListarProveedores.java
+++ SistemaDeCompras/src/sistemadecompras/vistas/ListarProveedores.java
@@ -1,22 +1,32 @@
 package sistemadecompras.vistas;
 
 import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import sistemadecompras.acceso.ProveedorData;
 import sistemadecompras.entidades.Proveedor;
 
 public class ListarProveedores extends javax.swing.JInternalFrame {
-
-    private final DefaultTableModel modelo;
+    private boolean cambios;
+    @SuppressWarnings("Convert2Lambda")
 
     public ListarProveedores() {
         initComponents();
         Menu m = new Menu();
         m.centrarInternalFrame(this);
-        this.modelo = new DefaultTableModel();
         llenarTabla();
+                ///////////////////////////////////////////////// ESTO ES PARA ACTIVAR EL BOTON DE MODIFICAR
+                         DefaultTableModel modelo = (DefaultTableModel) jtProveedores.getModel();
+                         modelo.addTableModelListener(new TableModelListener() {
+                         @Override
+                         public void tableChanged(TableModelEvent e) {
+                                 cambios = true;
+                                 checkCampos();                             
+                         }
+                     });
     }
-
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -25,8 +35,8 @@ public class ListarProveedores extends javax.swing.JInternalFrame {
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtProveedores = new javax.swing.JTable();
-        jbLimpiar = new javax.swing.JButton();
-        jbGuardar = new javax.swing.JButton();
+        jbModificar = new javax.swing.JButton();
+        jbEliminar = new javax.swing.JButton();
         jbSalir = new javax.swing.JButton();
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
@@ -40,26 +50,28 @@ public class ListarProveedores extends javax.swing.JInternalFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "idProveedor", "Nombre", "Direccion", "Telefono"
+                "idProveedor", "Nombre", "Telefono", "Direccion"
             }
         ));
+        jtProveedores.setColumnSelectionAllowed(true);
         jtProveedores.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(jtProveedores);
+        jtProveedores.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         if (jtProveedores.getColumnModel().getColumnCount() > 0) {
-            jtProveedores.getColumnModel().getColumn(3).setResizable(false);
+            jtProveedores.getColumnModel().getColumn(0).setMaxWidth(100);
         }
 
-        jbLimpiar.setText("Modificar");
-        jbLimpiar.addActionListener(new java.awt.event.ActionListener() {
+        jbModificar.setText("Modificar");
+        jbModificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbLimpiarActionPerformed(evt);
+                jbModificarActionPerformed(evt);
             }
         });
 
-        jbGuardar.setText("Eliminar");
-        jbGuardar.addActionListener(new java.awt.event.ActionListener() {
+        jbEliminar.setText("Eliminar");
+        jbEliminar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbGuardarActionPerformed(evt);
+                jbEliminarActionPerformed(evt);
             }
         });
 
@@ -78,30 +90,30 @@ public class ListarProveedores extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(123, 123, 123)
-                        .addComponent(jbLimpiar)
+                        .addComponent(jbModificar)
                         .addGap(38, 38, 38)
-                        .addComponent(jbGuardar)
+                        .addComponent(jbEliminar)
                         .addGap(43, 43, 43)
                         .addComponent(jbSalir))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(44, 44, 44)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(168, 168, 168)
+                        .addGap(173, 173, 173)
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(65, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(42, 42, 42)
+                .addGap(26, 26, 26)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
+                .addGap(46, 46, 46)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(51, 51, 51)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbLimpiar)
-                    .addComponent(jbGuardar)
+                    .addComponent(jbModificar)
+                    .addComponent(jbEliminar)
                     .addComponent(jbSalir))
                 .addContainerGap(29, Short.MAX_VALUE))
         );
@@ -109,34 +121,59 @@ public class ListarProveedores extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jbLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbLimpiarActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jbLimpiarActionPerformed
+    private void jbModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbModificarActionPerformed
 
-    private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
+        int filaSeleccionada = jtProveedores.getSelectedRow();
+
+        if (filaSeleccionada != -1) {
+            int idProveedor = (int) jtProveedores.getValueAt(filaSeleccionada, 0);
+            String nombreProveedor = (String) jtProveedores.getValueAt(filaSeleccionada, 1);
+            String direccion = (String) jtProveedores.getValueAt(filaSeleccionada, 2);
+            String telefono = (String) jtProveedores.getValueAt(filaSeleccionada, 3);
+
+            Proveedor proveedor = new Proveedor(idProveedor, nombreProveedor, direccion, telefono);
+            ProveedorData p = new ProveedorData();
+            p.modificarProveedor(proveedor);
+
+            llenarTabla();
+            checkCampos();
+        } else {
+
+            JOptionPane.showMessageDialog(this, "Por favor, seleccione un proveedor para modificar.", "Sin selección", JOptionPane.WARNING_MESSAGE);
+        }
+    }//GEN-LAST:event_jbModificarActionPerformed
+
+    private void jbEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbEliminarActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jbGuardarActionPerformed
+    }//GEN-LAST:event_jbEliminarActionPerformed
 
     private void jbSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSalirActionPerformed
         this.dispose();
     }//GEN-LAST:event_jbSalirActionPerformed
 
-
     private void llenarTabla() {
-        
+        DefaultTableModel modelo = (DefaultTableModel) jtProveedores.getModel();
+        modelo.setRowCount(0);
         ProveedorData p = new ProveedorData();
         List<Proveedor> lista = p.listarProveedores();
         lista.forEach((elemento) -> {
-      
             modelo.addRow(new Object[]{elemento.getIdProveedor(), elemento.getNombreProveedor(), elemento.getDomicilio(), elemento.getTelefono()});
         });
+        checkCampos();
     }
-     
+
+    private void checkCampos() {
+         if (cambios) {
+            jbModificar.setEnabled(true);
+        } else {
+            jbModificar.setEnabled(false);
+        }
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JButton jbGuardar;
-    private javax.swing.JButton jbLimpiar;
+    private javax.swing.JButton jbEliminar;
+    private javax.swing.JButton jbModificar;
     private javax.swing.JButton jbSalir;
     private javax.swing.JTable jtProveedores;
     // End of variables declaration//GEN-END:variables
