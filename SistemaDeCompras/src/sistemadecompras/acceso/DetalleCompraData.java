45ccc71a07f9bcbbb19f626995adb9f33c3d7856
victorbenitez012 <141685497+victorbenitez012@users.noreply.github.com>
2023-10-11 19:30:53 -0300

    DetalleCompraData
    Actualizacion de detalle compra data con otro constructor

--- SistemaDeCompras/src/sistemadecompras/acceso/DetalleCompraData.java
+++ SistemaDeCompras/src/sistemadecompras/acceso/DetalleCompraData.java
@@ -39,7 +39,25 @@ public class DetalleCompraData {
              Logger.getLogger(DetalleCompraData.class.getName()).log(Level.SEVERE, null, ex);
          }
     }
-    
+    public void agregarDetalleCompra(int cantidad, double precioCosto, int idCompra,int idProducto){
+        String sql = "INSERT INTO detallecompra (cantidad,	precioCosto,idCompra,idProducto	) VALUES (?,?,?,?)";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, cantidad );
+            ps.setDouble(2, precioCosto);
+            ps.setInt(3,idCompra);
+            ps.setInt(4, idProducto);
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Detalle cargado correctamente");
+            }
+            ps.close();
+           
+        } catch (SQLException ex) {
+             Logger.getLogger(DetalleCompraData.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
     public List<Compra> listarPorId(){
         List<Compra> comp = new ArrayList<>();
         try {
