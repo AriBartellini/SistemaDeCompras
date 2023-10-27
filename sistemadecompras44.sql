89145bb35e28b9576c2b1c300b36bf66cc082d71
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-10-27 19:29:41 -0300

    carga de sql

--- sistemadecompras44.sql
+++ sistemadecompras44.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2023 a las 01:07:36
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2023 at 12:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistemadecompras`
+-- Database: `sistemadecompras44`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `compra`
+-- Table structure for table `compra`
 --
 
 CREATE TABLE `compra` (
@@ -37,18 +37,17 @@ CREATE TABLE `compra` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `compra`
+-- Dumping data for table `compra`
 --
 
 INSERT INTO `compra` (`idCompra`, `idProveedor`, `detalle`, `cant`, `total`, `fecha`) VALUES
-(6, 7, '[2 - Heladera c/ freezer 200 lts]', 5, 750, '2023-10-26 23:03:44'),
-(7, 8, '[3 - Lavarropas carga frontal 10 kg]', 10, 582, '2023-10-26 23:03:59'),
-(9, 9, '[2 - Heladera c/ freezer 200 lts, 3 - Lavarropas carga frontal 10 kg, 4 - Lavavajillas ecologico]', 30, 9974, '2023-10-26 23:06:47');
+(1, 1, '[1 - Televisor]', 2, 4600, '2023-10-27 22:23:07'),
+(2, 2, '[2 - Foco 12v]', 60, 8000, '2023-10-27 22:26:23');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `detallecompra`
+-- Table structure for table `detallecompra`
 --
 
 CREATE TABLE `detallecompra` (
@@ -60,26 +59,17 @@ CREATE TABLE `detallecompra` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `detallecompra`
+-- Dumping data for table `detallecompra`
 --
 
 INSERT INTO `detallecompra` (`idDetalle`, `cantidad`, `precioCosto`, `idCompra`, `idProducto`) VALUES
-(1, 5, 375000, 1, 1),
-(2, 2, 150000, 2, 1),
-(3, 2, 150000, 3, 1),
-(4, 2, 150000, 4, 1),
-(5, 2, 150000, 5, 1),
-(6, 5, 2750, 6, 2),
-(7, 10, 6582, 7, 3),
-(8, 1, 789.2, 8, 4),
-(9, 10, 5500, 9, 2),
-(10, 10, 6582, 9, 3),
-(11, 10, 7892, 9, 4);
+(1, 2, 44600, 1, 1),
+(2, 60, 48000, 2, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `producto`
+-- Table structure for table `producto`
 --
 
 CREATE TABLE `producto` (
@@ -91,19 +81,17 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `producto`
+-- Dumping data for table `producto`
 --
 
 INSERT INTO `producto` (`idProducto`, `nombre`, `precio`, `stock`, `estado`) VALUES
-(1, 'microondas wave 3500w', 75000, 12, 0),
-(2, 'Heladera c/ freezer 200 lts', 550, 15, 1),
-(3, 'Lavarropas carga frontal 10 kg', 658.2, 20, 1),
-(4, 'Lavavajillas ecologico', 789.2, 11, 1);
+(1, 'Televisor', 22300, 2, 1),
+(2, 'Foco 12v', 800, 60, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `proveedor`
+-- Table structure for table `proveedor`
 --
 
 CREATE TABLE `proveedor` (
@@ -115,33 +103,26 @@ CREATE TABLE `proveedor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `proveedor`
+-- Dumping data for table `proveedor`
 --
 
 INSERT INTO `proveedor` (`idProveedor`, `nombre`, `domicilio`, `telefono`, `estado`) VALUES
-(1, 'a', 'b', 'c', 0),
-(3, 'Samsung', 'Calle 123', '44466655', 0),
-(4, 'Panasonic', 'Calle 555', '464646', 0),
-(5, 'Phillips', 'Avenida 456', '464646', 0),
-(6, 'Lalala', 'Calle ', '123', 0),
-(7, 'Samsung', 'Calle 123', '445566', 1),
-(8, 'Phillips', 'Av Calle 123', '4466655', 1),
-(9, 'Lenovo', 'Calle 456', '4466655', 1),
-(10, 'Gafa', 'Calle 444', '464646', 1);
+(1, 'LG', 'corrientes 3443', '3424442523', 1),
+(2, 'Gadnic', 'Centro 8300', '0800442332', 1);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `compra`
+-- Indexes for table `compra`
 --
 ALTER TABLE `compra`
   ADD PRIMARY KEY (`idCompra`),
-  ADD KEY `idProveedor` (`idProveedor`);
+  ADD KEY `mad` (`idProveedor`);
 
 --
--- Indices de la tabla `detallecompra`
+-- Indexes for table `detallecompra`
 --
 ALTER TABLE `detallecompra`
   ADD PRIMARY KEY (`idDetalle`),
@@ -149,61 +130,54 @@ ALTER TABLE `detallecompra`
   ADD KEY `idCompra` (`idCompra`);
 
 --
--- Indices de la tabla `producto`
+-- Indexes for table `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`idProducto`);
 
 --
--- Indices de la tabla `proveedor`
+-- Indexes for table `proveedor`
 --
 ALTER TABLE `proveedor`
   ADD PRIMARY KEY (`idProveedor`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `compra`
+-- AUTO_INCREMENT for table `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `detallecompra`
+-- AUTO_INCREMENT for table `detallecompra`
 --
 ALTER TABLE `detallecompra`
-  MODIFY `idDetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idDetalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `producto`
+-- AUTO_INCREMENT for table `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `proveedor`
+-- AUTO_INCREMENT for table `proveedor`
 --
 ALTER TABLE `proveedor`
-  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `compra`
+-- Constraints for table `compra`
 --
 ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`);
-
---
--- Filtros para la tabla `detallecompra`
---
-ALTER TABLE `detallecompra`
-  ADD CONSTRAINT `detallecompra_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
-  ADD CONSTRAINT `detallecompra_ibfk_2` FOREIGN KEY (`idCompra`) REFERENCES `compra` (`idCompra`);
+  ADD CONSTRAINT `mad` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
